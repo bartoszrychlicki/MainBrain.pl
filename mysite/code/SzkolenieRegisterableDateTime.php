7c1c3aea9480af0257f47cffefa2ff54cdcdcf67
unknown <br-design@Bartosz-PC.(none)>
2012-02-21 12:43:27 +0100

    plik dla szkolenia date time

---
+++ mysite/code/SzkolenieRegisterableDateTime.php
@@ -0,0 +1,8 @@
+<?php
+class SzkolenieRegisterableDateTime extends RegisterableDateTime {
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
